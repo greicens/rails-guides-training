25287c181a87008550e38fa0dbcfef723c96cc75
greicens <gsilva@mail.ccsf.edu>
2017-01-21 20:29:49 -0800

    adds a delete actin and links

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
   def show
     @article = Article.find(params[:id])
   end
@@ -33,6 +34,14 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
