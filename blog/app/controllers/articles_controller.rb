ab2ee17f52c3ec770bf23a3c8625471aec46e402
greicens <gsilva@mail.ccsf.edu>
2017-01-21 18:53:23 -0800

    creates a show view after form is submited

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save redirect_to @article
+    @article.save
+    redirect_to @article
   end
 
   private
